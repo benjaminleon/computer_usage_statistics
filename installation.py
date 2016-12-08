68690a401d4bdf93a04c810e4362af1abc62ba22
Benjamin <benli778@student.liu.se>
2016-12-08 14:56:14 +0100

    remove unnecessary printing, and add more informative print outs

--- installation.py
+++ installation.py
@@ -4,24 +4,28 @@
 import os
 cwd = os.getcwd()  # Current Working Directory
 
+print "Changing all occurences of PATH_TO_REPOSITORY to ", cwd
+
 # Set up the paths
 for filename in ["bash_script", "python_script.py", "print_to_terminal.py"]:
-    print filename
 
     with open(filename, 'r') as myfile:
         text = myfile.read()
 
-    print text
     new_text = text.replace("PATH_TO_REPOSITORY", cwd)
-    print new_text
 
     with open(filename, 'w') as myfile:
         myfile.write(new_text)
 
-
 # Initialize an empty histogram if there is none
-if not os.path.isfile(cwd + '/histogram.txt'):
+if os.path.isfile(cwd + '/histogram.txt'):
+    print "Histogram already present, leaving it untouched."
+    
+else:
+    print "No histogram.txt present, making a new empty histogram"
     empty_histogram = "0\n"*24
     with open('histogram.txt', 'w') as myfile:
         myfile.write(empty_histogram)
+    
+        
 
--- prepare_deploy.py
+++ prepare_deploy.py
@@ -1,18 +1,16 @@
 # prepare_deploy.py
-# Replaces all occurences of "/home/ben/Documents/" with the template path PATH_TO_REPOSITORY. This is used in the deploy and when installing, PATH_TO_REPOSITORY is replaced by the actual path to the repository folder.
+# Replaces all occurences of the actual path to the project folder with the template path PATH_TO_REPOSITORY. This is used in the deploy and when installing, PATH_TO_REPOSITORY is replaced by the actual path to the repository folder.
+
+import os
+cwd = os.getcwd()  # Current Working Directory
+
+print "Changing all occurences of ", cwd,  " to PATH_TO_REPOSITORY"
 
 for filename in ["bash_script", "python_script.py", "print_to_terminal.py"]:
-    print filename
-
     with open(filename, 'r') as myfile:
         text = myfile.read()
 
-
-    print text
-    new_text = text.replace("/home/ben/Documents/computer_usage_statistics/", "PATH_TO_REPOSITORY/")
-    print new_text
+    new_text = text.replace(cwd, "PATH_TO_REPOSITORY")
     
     with open(filename, 'w') as myfile:
         myfile.write(new_text)
-
-
